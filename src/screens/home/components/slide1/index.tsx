01138b50b4d7aab392d4e7c43acf07d5b071c8a6
teamchong <25894545+teamchong@users.noreply.github.com>
2022-08-19 06:31:27 +0000

    fix: typo Read-time Real-time

--- src/screens/home/components/slide1/index.tsx
+++ src/screens/home/components/slide1/index.tsx
@@ -3,7 +3,7 @@ import { StyledBox, StyledTypography1, StyledTypography2 } from './styles';
 
 const Slide1 = () => (
   <StyledBox>
-    <StyledTypography1 variant="h2">Read-time Interchain Explorer</StyledTypography1>
+    <StyledTypography1 variant="h2">Real-time Interchain Explorer</StyledTypography1>
     <StyledTypography2 variant="h4">
       View real-time status of different networks
     </StyledTypography2>
