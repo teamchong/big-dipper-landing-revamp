5cfc51aa9c1e0112803ad76d9c7e2ee0415262e4
teamchong <25894545+teamchong@users.noreply.github.com>
2022-08-22 14:55:44 +0800

    feat: add 404 page

--- src/screens/404/index.tsx
+++ src/screens/404/index.tsx
@@ -2,25 +2,21 @@
 import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import Typography from '@mui/material/Typography';
-import Layout from '@src/components/layout';
-import Link from 'next/link';
-import { StyledSectionBox } from './styles';
+import Image from 'next/image';
+import { StyledLayout } from './styles';
 
 const FourOhFour = () => {
   const { t } = useTranslation('common');
 
   return (
-    <Layout>
-      <StyledSectionBox>
-        <Typography className="title" variant="h4">
-          {t('404')}
-        </Typography>
-
-        <Link href="/">
-          <a>{t('goBackHome')}</a>
-        </Link>
-      </StyledSectionBox>
-    </Layout>
+    <StyledLayout>
+      <Image alt={t('404')} src="/images/404-unicorn.svg" width={380} height={380} />
+      <Typography variant="h3">
+        {t('404')}
+      </Typography>
+      <p> </p>
+      <Typography variant="h4">{t('404Description')}</Typography>
+    </StyledLayout>
   );
 };
 
--- src/screens/404/styles.tsx
+++ src/screens/404/styles.tsx
@@ -1,11 +1,30 @@
 import { styled } from '@mui/material/styles';
-import SectionBox from '@src/components/section-box';
+import Layout from '@src/components/layout';
 
-export const StyledSectionBox = styled(SectionBox)(({ theme }) => ({
-  display: 'flex',
-  flexFlow: 'column nowrap',
-  minHeight: theme.spacing(50),
-  gap: theme.spacing(5),
-  justifyContent: 'center',
-  alignItems: 'center',
+export const StyledLayout = styled(Layout)(({ theme }) => ({
+  '& > div > div:nth-of-type(2)': {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+  '& img': {
+    marginTop: '-20px !important',
+    marginBottom: '-100px !important',
+  },
+  '& h3': {
+    fontSize: '1.2rem',
+  },
+  '& h4': {
+    fontSize: '0.95rem',
+  },
+  [theme.breakpoints.up('md')]: {
+    '& h3': {
+      fontSize: '2rem',
+    },
+    '& h4': {
+      fontSize: '1.4rem',
+    },
+  },
 }));
